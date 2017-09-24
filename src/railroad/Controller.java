8084247c40c9d6dd55765c44d143093e4e7f9d56
Felipe Pessoa <felipessoaf@gmail.com>
2017-09-24 19:26:26 -0300

    controller singleton

--- src/railroad/Controller.java
+++ src/railroad/Controller.java
@@ -1,5 +1,21 @@
 package railroad;
 
-class Controller {
+class Controller 
+{
+	static private Controller _controller;
+		
+	private Controller()
+	{
+		
+	}
 
+	static public Controller getInstance()
+	{
+		if(_controller == null)
+		{
+			_controller = new Controller();
+		}
+		
+		return _controller;
+	}
 }
